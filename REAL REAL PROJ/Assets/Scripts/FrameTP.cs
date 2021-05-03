ec11021aca478214ad08c53842e892405e33258f
Gorungas <50162737+Gorungas@users.noreply.github.com>
2021-05-03 10:16:05 -0700

    REBASE FOR GROUP PROJ!!!

---
+++ REAL REAL PROJ/Assets/Scripts/FrameTP.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameTP : MonoBehaviour
+{
+    public Transform tpOut;
+    public GameObject player;
+    public Camera cam1;
+    public Transform camTP;
+    public Rigidbody rb;
+    public CamTrack tracker;
+    public PlayerMovement mover;
+    public bool hasBeenEntered = false;
+
+    public LossScript loss;
+
+    public Material skybox2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam1.enabled = true;
+        rb = player.GetComponent<Rigidbody>();
+        tracker = cam1.GetComponent<CamTrack>();
+        mover = player.GetComponent<PlayerMovement>();
+
+
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (loss.level == -1)
+            {
+                player.transform.position = tpOut.transform.position;
+                cam1.transform.position = camTP.position;
+                RenderSettings.skybox = skybox2;
+                rb.constraints = RigidbodyConstraints.FreezePositionX;
+
+
+
+                tracker.track2d = true;
+                mover.move2d = true;
+
+
+                rb.angularVelocity = Vector3.zero;
+                rb.velocity = Vector3.zero;
+
+
+            }
+            if (!hasBeenEntered)
+            {
+                loss.level++;
+                hasBeenEntered = true;
+            }
+            
+        }
+    }
+
+}
---
+++ REAL REAL PROJ/Assets/Scripts/LossScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LossScript : MonoBehaviour
+{
+    public int level;
+
+    public Transform level1TP;
+    public Transform level2TP;
+    public Transform level3TP;
+
+    public GameObject player;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Cloud") || other.gameObject.CompareTag("Beak"))
+        {
+            print("coolididi");
+
+            switch (level)
+            {
+                case 0:
+                    player.transform.position = level1TP.position;
+                    break;
+                case 1:
+                    player.transform.position = level2TP.position;
+                    break;
+                case 2:
+                    player.transform.position = level3TP.position;
+                    break;
+            }
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
