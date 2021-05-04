449a965e83bd030074e39f716f526ea990fcb565
m-holtz <74580132+m-holtz@users.noreply.github.com>
2021-05-03 23:33:53 -0300

    Cut Scene SpaceBar change

---
+++ REAL REAL PROJ/Assets/Scripts/CutSceneButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutSceneButton : MonoBehaviour
+{
+    public KeyCode _Key;
+    private Button _button;
+
+
+    void Awake()
+    {
+        _button = GetComponent<Button>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(_Key))
+        {
+            _button.onClick.Invoke();
+        }
+    }
+}
