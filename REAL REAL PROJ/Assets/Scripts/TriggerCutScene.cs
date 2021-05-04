e99c9b597c31a6bb8d92b8ad859ca7b61583f5ec
m-holtz <74580132+m-holtz@users.noreply.github.com>
2021-05-04 00:06:44 -0300

    Trigger Cut Scenes

---
+++ REAL REAL PROJ/Assets/Scripts/TriggerCutScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TriggerCutScene : MonoBehaviour
+{
+
+    public GameObject CutScene;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            CutScene.gameObject.SetActive(true);
+        }
+    }
+
+}
