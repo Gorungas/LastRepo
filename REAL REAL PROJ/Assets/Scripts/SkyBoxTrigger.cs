e6392db287dd0a10a7e5bf11723357c2520a1adf
m-holtz <74580132+m-holtz@users.noreply.github.com>
2021-05-04 21:13:32 -0300

    audio to scene 0+2, finished cube World, BG transitions

---
+++ REAL REAL PROJ/Assets/Scripts/SkyBoxTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxTrigger : MonoBehaviour
+{
+    public Material otherSkybox;
+
+    public GameObject oldBG;
+    public GameObject newBG;
+
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            RenderSettings.skybox = otherSkybox;
+
+            Destroy(oldBG);
+
+            newBG.SetActive(true);
+        }
+    }
+}
